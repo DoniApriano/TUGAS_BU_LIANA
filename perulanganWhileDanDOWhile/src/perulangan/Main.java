c8fd616f057c08155bc5882aa34ecb99d7bde4ed
DoniApriano <107307637+DoniApriano@users.noreply.github.com>
2022-11-21 21:07:23 +0700

    Jawa cuyy

---
+++ perulanganWhileDanDOWhile/src/perulangan/Main.java
@@ -0,0 +1,22 @@
+package perulangan;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+/*        while (i < 10) {
+            System.out.println( "Aku ngganteng"  + i);
+            i++;
+        }*/
+
+
+        do {
+            System.out.println("Aku sangat ngganteng" + i);
+            i++;
+        } while (i < 10);
+
+
+    }
+
+}
