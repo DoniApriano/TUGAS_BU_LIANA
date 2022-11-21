35841f3ac6de0136a0ce21f02f6ec43e7ae4b74f
DoniApriano <107307637+DoniApriano@users.noreply.github.com>
2022-11-21 21:02:39 +0700

    Java cuyy

---
+++ perulanganFor/src/perulangan/Main.java
@@ -0,0 +1,20 @@
+package perulangan;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*for (int i = 0; i < 10; i++) {
+            System.out.println("Aku ngganteng");
+        }*/
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println();
+            for (int j = 0; j < 6; j++) {
+                System.out.print(j);
+            }
+        }
+
+    }
+
+}
