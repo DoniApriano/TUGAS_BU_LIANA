fe94aaede77e065a4d106ac040247af85ae9b20f
DoniApriano <107307637+DoniApriano@users.noreply.github.com>
2022-11-24 10:53:26 +0700

    Java

---
+++ Tipe data/src/tipedata/Main.java
@@ -0,0 +1,22 @@
+package tipedata;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int angka = 10;
+        double desimal = 10.5;
+        char character = 'A';
+        float floatDesimal = 3.5f;
+        boolean booleanTp = true;
+        String kalimat = "Halo saia doni apriano";
+
+
+        System.out.println(angka);
+        System.out.println(desimal);
+        System.out.println(booleanTp);
+        System.out.println(character);
+        System.out.println(kalimat);
+        System.out.println(floatDesimal);
+    }
+
+}
